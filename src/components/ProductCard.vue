8d6780e4f369be40cba14b145d67cea1348cf638
Hairul <98758226+HairulDev@users.noreply.github.com>
2024-08-14 12:04:28 +0700

    fix: button product card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -29,16 +29,16 @@
       </div>
     </div>
     <div class="px-6 py-2 flex items-center justify-between">
-      <button @click="openDetailModal" class="bg-[#b5179e] text-white font-bold py-2 px-4 rounded">
+      <button @click="openDetailModal" class="green-text-gradient font-bold py-2 px-4 rounded">
         Detail
       </button>
-      <button @click="addToCart" class="bg-[#915EFF] text-white font-bold py-2 px-6 rounded">
+      <button @click="addToCart" class="blue-text-gradient font-bold py-2 px-6 rounded">
         <svg width="23px" height="23px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g id="SVGRepo_bgCarrier" stroke-width="0"/>
         <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
         <g id="SVGRepo_iconCarrier"> 
           <path d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" 
-            stroke="#ffffff" 
+            stroke="#56ccf2" 
             stroke-width="2" 
             stroke-linecap="round" 
             stroke-linejoin="round"/> 
